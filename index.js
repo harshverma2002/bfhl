9df83db05f60efe788c296105252b8f6cde1aab1
Harsh <vermaharsh836@gmail.com>
2024-08-25 12:26:35 +0530

    2commit

--- index.js
+++ index.js
@@ -2,7 +2,11 @@ const express = require("express");
 const app = express();
 
 app.use(express.json());
-
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
 app
   .route("/bfhl")
   .get((req, res) => {
